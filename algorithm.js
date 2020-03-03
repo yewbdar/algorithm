96e1444c74b1602a94bb368def7830f40fb5aff6
yewbdar <yewbdar.grima@Gmail.com>
2020-03-03 11:48:06 -0600

    find the longest word

--- algorithm.js
+++ algorithm.js
@@ -1,4 +1,4 @@
-// CHAKKENGE 1 : reverse a string
+// CHALLENGE 1 : reverse a string
 
 //e.g reverseString('hello') = 'olleh'
 
@@ -7,6 +7,27 @@ const reverseString = (str) => {
     return str.split('').reverse().join('')
 };
 
-console.log(reverseString('hello'));
+// console.log(reverseString('hello'));
 
 
+//CHALLENGE 2 : longest word
+// eg longestWord('hello, may name is yewbdar') === 'yewbdar'
+
+const longestWord = (sen) => {
+    //get only character
+    let wordChar= sen.match(/[a-z0-9]+/g);
+
+    wordChar.sort((a,b) => b.length - a.length);
+
+    let longestWord;
+    longestWord = wordChar.filter(item => item.length === wordChar[0].length);
+
+    if(longestWord.length === 1){
+        return wordChar[0];
+
+    }else {
+        return longestWord;
+    }
+    // console.log(longestWord)
+};
+console.log(longestWord('helloii, my name is yewbdar'));
