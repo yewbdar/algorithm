54d75a8ff7cc173d9878af7c42e9139edc34c617
yewbdar <yewbdar.grima@Gmail.com>
2020-03-03 10:05:06 -0600

    reverse a string

---
+++ algorithm.js
@@ -0,0 +1,12 @@
+// CHAKKENGE 1 : reverse a string
+
+//e.g reverseString('hello') = 'olleh'
+
+
+const reverseString = (str) => {
+    return str.split('').reverse().join('')
+};
+
+console.log(reverseString('hello'));
+
+
