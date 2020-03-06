62b6ce09eddbf0d29146c9710c60a1574fd56633
yewbdar <yewbdar.grima@Gmail.com>
2020-03-06 11:29:41 -0600

    change to capital letter the

--- algorithm.js
+++ algorithm.js
@@ -30,4 +30,35 @@ const longestWord = (sen) => {
     }
     // console.log(longestWord)
 };
-console.log(longestWord('helloii, my name is yewbdar'));
+
+
+//CHALLENGE 3 : PALINDROME
+// eg : isPaledrome('racecar') === 'true'
+
+const isPaledrome = ( str) =>{
+
+   let newStr= str.split('').reverse().join('');
+
+  return newStr === str
+
+}
+
+// CHALLENGE 4 : REVERSE AN INTEGER
+
+const reverseInt = (num) => {
+     let newNum =  num.toString().split('').reverse().join('');
+
+     return parseInt(newNum) * Math.sign(num)
+}
+
+//CHALLENGE 5 : CAPITALIZE LETTERS
+
+// eg capitalizeLatter('i love you') = I Love You
+
+const capitalizeLatter = (str) => {
+    let newStr = str.split(' ').map(word => word.substring(0,1).toUpperCase() + word.substring(1));
+    return newStr.join(' ');
+}
+
+console.log(capitalizeLatter('i love you'));
+
